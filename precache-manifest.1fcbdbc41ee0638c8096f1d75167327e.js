2a7859094b01c6a500ff148218ce579dd7126ebc
Fernando Augusto Rodrigues de Araujo <adrafdev@MacBook-Pro-de-Fernando.local>
2020-04-05 08:57:52 -0300

    Updates

---
+++ precache-manifest.1fcbdbc41ee0638c8096f1d75167327e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b23ba7dde6195815fbae970e1016d9ed",
+    "url": "/chillbass/index.html"
+  },
+  {
+    "revision": "95e8ba6fce6cad64159f",
+    "url": "/chillbass/static/js/2.ba1609e5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/chillbass/static/js/2.ba1609e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d6772613172ec89588b",
+    "url": "/chillbass/static/js/main.68ae8011.chunk.js"
+  },
+  {
+    "revision": "5e00dbb78402452e59bf",
+    "url": "/chillbass/static/js/runtime-main.27c6faa0.js"
+  }
+]);
